58877c20332729c7d961e926fcdbb3605bc64884
duynt <duynt@DESKTOP-IJ2V8I3>
2026-01-18 06:40:52 +0700

    added profile page, fixed dropdown button on Avatar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from './providers/theme-provider'
 import LoginPage from './features/auth/pages/login-page'
 import RegisterPage from './features/auth/pages/register-page'
 import ChatPage from './features/chat/pages/chat-page'
+import UserProfilePage from './features/user-profile/pages/user-profile-page'
+
 function App() {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
@@ -13,11 +15,10 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path='/chat' element={<ChatPage/>}></Route>
-
+          <Route path='/profile' element={<UserProfilePage/>}></Route> 
         </Routes>
       </Router>
     </ThemeProvider>
-    
   )
 }
 
