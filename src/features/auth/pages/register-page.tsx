7bf0c93580c58176b7caf67713c9800c415cb775
HoTung <hoviettung2@gmail.com>
2026-01-29 19:59:26 +0700

    fix: change background color to gradient

--- src/features/auth/pages/register-page.tsx
+++ src/features/auth/pages/register-page.tsx
@@ -1,7 +1,7 @@
 import { RegisterForm } from "../components/register-form"
 export default function RegisterPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <div className="bg-gradient-to-br from-primary via-secondary to-muted bg-opacity-80 flex min-h-screen flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
         <RegisterForm/>
       </div>
--- src/features/user/sidebar/sidebar-wrapper.tsx
+++ src/features/user/sidebar/sidebar-wrapper.tsx
@@ -4,7 +4,7 @@ import MobileNav from "./mobile-nav";
 type Props = React.PropsWithChildren<{}>;
 export default function SidebarWrapper({children}: Props){
     return(
-        <div className="h-screen w-full overflow-hidden p-4 flex flex-col lg:flex-row gap-4">
+        <div className="bg-gradient-to-br from-primary via-secondary to-muted bg-opacity-80 h-screen w-full overflow-hidden p-4 flex flex-col lg:flex-row gap-4 ">
             <MobileNav/>
             <DesktopNav/>
             <main className="h-[calc(100%-80px)] lg:h-full w-full flex gap-4">
